ea8bd31cd1ac67e8ebb575efc2376b6b3e5f945d
Usham Roy <129624990+uroy80@users.noreply.github.com>
2024-12-25 11:53:54 +0530

    Navigation bar design [Step-1]

--- home.php
+++ home.php
@@ -3,16 +3,23 @@
     <title>
     Home Page
     </title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 <header>
-    <div>
-        <div><a href="home.php">Home</a> </div>
-        <div><a href="#">Gallery</a> </div>
-        <div><a href="#">Booking</a> </div>
-        <div><a href="#">Register</a> </div>
-        <div><a href="contact.php">Contact Us</a></div>
+    <div class="navbar">
+        <div class="txt">
+            <span id="p1">Happy Tour & Travels</span><br>
+            <span id="p2">Life in Color</span>
+            <span id="img"><img src="lve.jpg" height="30" width="30"> </span>
+        </div>
+        <div class="nb1"><a href="home.php" class="nba">Home</a> </div>&nbsp;
+        <div class="nb2"><a href="#" class="nba">Gallery</a> </div>&nbsp;
+        <div class="nb3"><a href="#" class="nba">Booking</a> </div>&nbsp;
+        <div class="nb4"><a href="#" class="nba">Register</a> </div>&nbsp;
+        <div class="nb5"><a href="contact.php" class="nba">Contact Us</a></div>&nbsp;
     </div>
 </header>
 </body>
